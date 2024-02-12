50c1679271bdff02ba1862fe55f01b9fd924f77f
niloofar <mousavi.n0@gmail.com>
2024-02-12 09:45:47 +0100

    simple menu

--- app.py
+++ app.py
@@ -1,1 +1,27 @@
-#HELLO GIT!
+def prepare_relational_database():
+    pass
+
+
+
+
+
+
+
+
+
+if(__name__ == "__main__"):
+    prepare_relational_database()
+    while True:
+        menu = input("""Välj ett alternative:
+                     1. Bli medlem 
+                     2. Logga in 
+                     3. Stäng Appen! \n """)
+
+        if (menu == '1'):
+            pass
+        elif (menu == '2'):
+            pass
+        elif (menu == '3'):
+            break
+        else:
+            print("Ingen gilgit inmatning! Försök igen!") 
