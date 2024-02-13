68a2b327338884e30b6f8f4a83f19992b4d3ff81
niloofar <mousavi.n0@gmail.com>
2024-02-13 10:26:05 +0100

    main menu

--- app.py
+++ app.py
@@ -1,26 +1,6 @@
-from sqlite_commands import prepare_relational_database
-from helper_functions import prepare_log_files
-from menu_functions import login_menu, create_account_menu
+from menu_functions import main_menu
 
-
-def main():
-    prepare_relational_database()
-    prepare_log_files()
-    while True:
-        menu = input("""Välj ett alternative:
-                     1. Bli medlem 
-                     2. Logga in 
-                     3. Stäng Appen! \n """)
-
-        if menu == '1':
-            create_account_menu()
-        elif menu == '2':
-            login_menu()
-        elif menu == '3':
-            break
-        else:
-            print("Ingen gilgit inmatning! Försök igen!") 
 
 
 if __name__ == "__main__":
-    main()
+    main_menu()
