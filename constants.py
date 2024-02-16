ddc6b615a22ec659fd115424e7e95dfd11828f40
niloofar <mousavi.n0@gmail.com>
2024-02-16 18:54:46 +0100

    schedule the etl process every one hour

--- constants.py
+++ constants.py
@@ -2,7 +2,7 @@ login_log_file = 'user_login.csv'
 login_summary_file = 'login_history.xlsx'
 
 field_sv_to_column_name= {'användarnamn':'username', 'lösenord':'password_hash','förnamn':'firstname','efternamn':'lastname','adress':'address','telefonnummer':'telephone'}
-schedule_seconds = 10
+schedule_seconds = 86400
 
 mongodb_url = "mongodb://localhost:27017/"
 
