0b23ff949ce1c19ee20826860fc97bcd70971ea2
niloofar <mousavi.n0@gmail.com>
2024-02-16 12:25:54 +0100

    remove log file after updating history

--- etl_app.py
+++ etl_app.py
@@ -1,13 +1,16 @@
 from constants import schedule_seconds
-from helper_functions import read_from_log_file_write_to_table, update_summary_file
-
+from helper_functions import read_from_log_file_write_to_table, update_summary_file, get_login_summary
+from sqlite_commands import create_users_logins_tables
 import time
-import sys
+import os
+from constants import login_log_file
 
 def etl_loop():
     try:
         while True:
+            create_users_logins_tables()
             read_from_log_file_write_to_table()
+            os.remove(login_log_file)
             update_summary_file()
             time.sleep(schedule_seconds)
     except KeyboardInterrupt:
@@ -15,3 +18,4 @@ def etl_loop():
 
 if __name__ == '__main__':
     etl_loop()
+    # print(get_login_summary())
